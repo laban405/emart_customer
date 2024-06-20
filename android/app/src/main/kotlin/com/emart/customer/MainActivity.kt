ddd7ce55bf7f2f9bdcd1575d3b43ac4edffdf94e
laban405 <laban.mogire@kamsware.com>
2024-06-20 16:41:34 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/emart/customer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.agrigo.customer
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity()
